d959efd6c80e9148b4ec0edc223d5980e4e73687
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-12-03 09:59:34 -0700

    server basics

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("I connected to a server!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log($"There are now {numPlayers} players");
+    }
+
+}
